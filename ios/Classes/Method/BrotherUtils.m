c8536b94c29036ba172ddabde4ec7216334e3ce0
Ralph Pon-an <ralph@thinkbitsolutions.com>
2026-07-18 00:01:53 +0800

    chore: replaced the DOUBLE_SPEED to be BRLMPrintSettingsResolutionNormal instead of Low

--- ios/Classes/Method/BrotherUtils.m
+++ ios/Classes/Method/BrotherUtils.m
@@ -378,7 +378,7 @@ static NSObject<FlutterPluginRegistrar>* _registrarFlutter;
         return BRLMPrintSettingsResolutionNormal;
     }
     else if ([@"DOUBLE_SPEED" isEqualToString:name]) {
-        return BRLMPrintSettingsResolutionLow;
+        return BRLMPrintSettingsResolutionNormal;
     }
     else if ([@"HIGH_RESOLUTION" isEqualToString:name]) {
         return BRLMPrintSettingsResolutionHigh;
@@ -729,7 +729,7 @@ static NSObject<FlutterPluginRegistrar>* _registrarFlutter;
     
     printerSettings.labelSize = [BrotherUtils qlLabelSizeWithName:labelName];
     printerSettings.autoCut = [[map objectForKey:@"isAutoCut"] isEqual:@(YES)];
-    printerSettings.cutAtEnd = [[map objectForKey:@"isEndCut"] isEqual:@(YES)];
+    printerSettings.cutAtEnd = [[map objectForKey:@"isEndCut"] isEqual:@(YES)] || [[map objectForKey:@"isCutAtEnd"] isEqual:@(YES)];
     printerSettings.resolution = [BrotherUtils printResolutionFromMapWithValue:dartPrintQuality];
     
     // TODO Extract info from map.
